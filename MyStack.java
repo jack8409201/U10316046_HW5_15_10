f43834d62c0289aaef171b9e5e4668c0af8fa9d3
jack8409201 <jack8409201@gmail.com>
2015-04-26 11:34:01 +0800

    Modify MyStack class
    新增註解

--- MyStack.java
+++ MyStack.java
@@ -1,31 +1,37 @@
 /**
  * Name: Jou Dan-Haur
  * ID: U10316046
- * Ex: 4.23
+ * Ex: 15.10
  */
 import java.util.ArrayList;
 
 public class MyStack implements Cloneable{
+	//Create an ArrayList store objects
 	public ArrayList<Object> list = new ArrayList<Object>();
-
+	
+	//Bollean method if stack is empty that return true
 	public boolean isEmpty() {
 		return list.isEmpty();
 	}
 
+	//Method return amount of element in the stack
 	public int getSize() {
 		return list.size();
 	}
 
+	//Method return top object of the stack but not delete
 	public Object peek() {
 		return list.get(getSize() - 1);
 	}
 
+	//Method delete top object in the stack and return it
 	public Object pop() {
 		Object o = list.get(getSize() - 1);
 		list.remove(getSize() - 1);
 		return o;
 	}
 
+	//Method store object on the top 
 	public void push(Object o) {
 		list.add(o);
 	}
@@ -34,6 +40,8 @@ public class MyStack implements Cloneable{
 	public String toString() {
 		return "stack: " + list.toString();
 	}
+	
+	//Deep copy  ArrayList Object
 	public Object clone(){
 		try{
 			MyStack MyStackClone = (MyStack)super.clone();
